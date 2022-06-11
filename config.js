19fb3e7bdace2091af5f86a700eecc29d5cfc54b
Satria356 <memeindo74@gmail.com>
2022-06-11 21:50:51 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,47 @@
+const fs = require("fs")
+const chalk = require("chalk")
+
+//Others
+global.owner = ['18312576749','6283811034750']
+global.nomerowner = "18312576749"
+global.ownername = "Fardev78"
+global.namebot = "ANA~BOT"
+global.sessionName = "session"
+global.log0 = fs.readFileSync("./media/image/thumb.jpg")
+global.qris = fs.readFileSync("./media/image/qris.jpg")
+global.fonts = "© ANA~BOT"
+
+//Exif
+global.packname = "Created by"
+global.author = "Ana kawai"
+
+//Message
+global.mess = {
+success: '*Success!*',
+admin: '*Fitur Khusus Admin Group!*',
+botAdmin: '*Bot Harus Menjadi Admin Terlebih Dahulu!*',
+owner: '*Fitur Khusus owner ana bot*',
+group: '*Fitur Digunakan Hanya Untuk Group!*',
+private: '*Fitur Digunakan Hanya Untuk Private Chat!*',
+bot: '*Fitur Khusus Pengguna Nomor Bot*',
+wait: '⏳ *_Wait ana bot is processing..._*',
+ban: '*Kamu telah dibanned oleh owner, Jika ingin di unbanned chat owner*'
+}
+
+//Pengguna
+global.rkyt = []
+
+//Limit
+global.limitawal = {
+premium: "Infinity",
+free: 20
+}
+
+//Pembatas
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
---
+++ message/help.js
@@ -0,0 +1,68 @@
+/**
+ - NEO BASE
+**/
+
+exports.donate = (prefix, I, pushname) => {
+return `
+—————「 *DONATE* 」—————
+
+Hai ${pushname}👋
+Kalian bisa mendukung saya agar bot ini tetap up to date dengan cara donasi
+Berapapun donasi kalian akan sangat berarti 👍
+
+Dana : 083811034750
+Ovo : 083168004413
+Pulsa : 083811034750
+Gopay : 083168004413
+
+Thanks!
+`
+}
+
+var SK = '⊳'
+var SB = '•'
+var HB = '❑'
+var K1 = '『'
+var K2 = '』'
+var YR = '⇒'
+var BF = '_'
+var P4 = '*'
+var GK = '➛'
+var DS = '┌─ꕺ'
+
+var monospace = '```'
+
+exports.help = (prefix, l) => {
+	return `
+─────────────────
+${HB} ${P4}Convert Menu${P4}
+${SB} ${prefix}nulis
+${SB} ${prefix}sticker
+${SB} ${prefix}ssweb
+${SB} ${prefix}teksto
+${SB} ${prefix}tahta
+
+${HB} ${P4}Search Menu${P4}
+${SB} ${prefix}loli
+${SB} ${prefix}neko
+${SB} ${prefix}waifu
+${SB} ${prefix}covidindo
+
+${HB} ${P4}Download Menu${P4}
+${SB} ${prefix}play
+${SB} ${prefix}tiktok
+${SB} ${prefix}ytmp3
+${SB} ${prefix}ytmp4
+${SB} ${prefix}tiktokaudio
+${SB} ${prefix}mediafire
+
+${HB} ${P4}Owner Menu${P4}
+${SB} ${prefix}creategc
+${SB} ${prefix}cowner
+${SB} ${prefix}bcall
+${SB} ${prefix}bcgc
+${SB} ${prefix}setexif
+${SB} ${prefix}setbiobot
+
+*ANA-Md@^1.0.0*\n${monospace}Simple Bot WhatsApp With Nodejs${monospace}`
+}
